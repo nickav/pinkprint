6ed6a3210e1ec4a6f0a0d23e88229cb264f30eeb
Nick Aversano <nick23hi@gmail.com>
2017-04-15 14:54:11 -0700

    add tests, command shell

--- index.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-const program = require('commander')
-const chalk = require('chalk')
-const app = require('./lib')
---
+++ test/index.js
@@ -0,0 +1,7 @@
+const assert = require('assert')
+
+describe('blueprint', function() {
+  it('should work', function() {
+    assert.equal(true, false)
+  })
+})
