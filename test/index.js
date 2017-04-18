4b823e852160073d23692c095318f9c26d7477a7
Nick Aversano <nick23hi@gmail.com>
2017-04-17 20:55:22 -0700

    add prettier and format files

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 /* eslint-env mocha */
 const assert = require('assert')
 
-describe('blueprint', function () {
-  it('should work', function () {
+describe('blueprint', function() {
+  it('should work', function() {
     assert.equal(true, true)
   })
 })
