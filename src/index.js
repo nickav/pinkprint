dcd73f39055d2ff46426b5730b1f89f8f8b4bbad
Nick Aversano <nick23hi@gmail.com>
2019-12-22 17:19:13 -0500

    got modules working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-export default () => console.log('hi');
+export const runNew = (argv) => {
+  console.log('not implemented');
+};
+
+export const runList = (argv) => {
+  console.log('not implemented');
+};
+
+export const runGenerate = (argv) => {
+  console.log('not implemented');
+};
